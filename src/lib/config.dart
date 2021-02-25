229818b4392801fed4c7e4f790f804809a3e48f8
Ryan Choi <choi.link227@gmail.com>
2021-02-25 17:00:35 -0500

    back to http

--- src/lib/config.dart
+++ src/lib/config.dart
@@ -23,7 +23,7 @@ Future<String> loadInterxURL() async {
   String url = await loadConfig();
 
   if (url.contains('http') == false) {
-    url = "https://" + url;
+    url = "http://" + url;
   }
 
   return url + "/api";
